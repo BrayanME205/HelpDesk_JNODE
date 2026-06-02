14e36eaea16b015ed9070869d5da7108e046ec14
Darío Mora Piedra <morapiedradari32@gmail.com>
2026-06-01 21:35:33 -0600

    Revert "CU4.2 - Ingreso de Solicitud por el Cliente"
    This reverts commit af9f56add4649652c09e0b2b9c3869454a0baf4a.

--- ConneXtion-Backend/src/main/java/com/example/demo/model/entities/IssueClassification.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.model.entities;
-
-
-public enum IssueClassification {
-    ALTA,
-    MEDIA,
-    BAJA
-}
-
--- ConneXtion-Backend/src/main/java/com/example/demo/model/entities/IssueStatus.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.model.entities;
-
-public enum IssueStatus {
-    INGRESADO,
-    ASIGNADO,
-    EN_PROGRESO,
-    RESUELTO
-}
